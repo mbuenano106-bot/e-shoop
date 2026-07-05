d5c6c8e4c79d812b1af7c99ee141e9d8083a0968
Mateo Buenaño <mbuenanor@est.ups.edu.ec>
2026-07-05 16:00:03 -0500

    feat: agregar modelos de productos, categorias y pedidos

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     migrate.init_app(app, db)
 
     # Modelos
-    from app.models import Usuario
+    from app.models import Usuario, Categoria, Producto , Pedido
 
     # Blueprints
     from app.blueprints.public import public_bp
--- app/config.py
+++ app/config.py
@@ -2,14 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
     # Configuración de la BD
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    )
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ecommerce_user:0963045@localhost/ecommerce_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     
